6e679fce7cf0561f891c8cdea833bfb798e23e27
Victor <victormwendwa804@gmail.com>
2024-08-02 08:50:23 +0300

    Blog post

--- Flask_Blog/flask_blog.py
+++ Flask_Blog/flask_blog.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'auhor': 'Victor Mwendwa',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 02, 2024'
+    },
+    {
+        'auhor': 'Ragnar Rock',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 03, 2024'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
