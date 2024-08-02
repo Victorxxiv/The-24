147af1676471d998b975530ac12503fab78d120b
Victor <victormwendwa804@gmail.com>
2024-08-02 05:11:53 +0300

    Queue

---
+++ Queue.py
@@ -0,0 +1,24 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, data):
+        if not self.is_empty():
+            return self.queue.pop(0)
+        return None
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.pop(0)
+        return None
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+
+# Example
+q = Queue()
+q.enqueue = (20)
+q.enqueue = (30)
+print(q.dequeue())  # 20
+print(q.is_empty())  # False
